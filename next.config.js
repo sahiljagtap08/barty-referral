e7545f22b9c0482af783cfac38a1424179615d2f
Sahil Jagtap <your-email@example.com>
2025-05-10 14:11:19 -0400

    Fix: Completely disable ESLint during build to fix deployment issues

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ import("./env.mjs");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Disable ESLint during builds
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
